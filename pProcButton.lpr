996ed5b34c243bfbc96399a81d8d5b3f04fb1917
Rastrelly <rastrelly@gmail.com>
2023-04-12 15:34:24 +0300

    Initial commit

---
+++ pProcButton.lpr
@@ -0,0 +1,25 @@
+program pProcButton;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, uprocbutton
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ uprocbutton.pas
@@ -0,0 +1,74 @@
+unit uprocbutton;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    procedure ButtonAction(Sender:TObject);
+    procedure makeAButton(var btn:TButton);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  newBut:TButton;
+  nPan:TPanel;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.ButtonAction(Sender:TObject);
+begin
+  ShowMessage('I am clicked!');
+end;
+
+procedure TForm1.makeAButton(var btn:TButton);
+begin
+
+  nPan:=TPanel.Create(self);
+  with nPan do
+  begin
+    Left:=10;
+    Top:=10;
+    Width:=Form1.ClientWidth-20;
+    Height:=Form1.ClientHeight-20;
+    Parent:=self;
+    Anchors:=[akLeft,akTop,akRight,akBottom];
+  end;
+
+  btn:=TButton.Create(self);
+  with btn do
+  begin
+    Parent:=nPan;
+    Left:=10;
+    Top:=10;
+    Width:=200;
+    Height:=25;
+    Caption:='I, Button';
+    OnClick:=@ButtonAction;
+  end;
+end;
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  makeAButton(newBut);
+end;
+
+end.
+
